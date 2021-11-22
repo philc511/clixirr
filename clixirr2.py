4f5d4eacb8f081b33752361195976c94af05d728
philc511 <phil.capon@fastmail.co.uk>
2021-11-22 20:15:53 +0000

    working tests

--- clixirr2.py
+++ clixirr2.py
@@ -4,16 +4,19 @@ import csv, datetime, financial
 
 def get_dates(cashflows, balances, fun):
     results = []
-    if cashflows:
+    if cashflows and len(set([b[0] for b in balances] + [c[0] for c in cashflows])) > 1:
         start_date = cashflows[0][0]
-        start_balance = 0.0
-        for txn in balances:
-            cf = [t for t in cashflows if t[0] >= start_date and t[0] <= txn[0]]
-            cf.append([start_date, start_balance])
-            cf.append(txn)
-            results.append([start_date, txn[0], fun(cf)])
-            start_date = txn[0]
-            start_balance = -txn[1]
+        end_dates = list(set([b[0] for b in balances]))
+        end_dates.sort()
+
+        for end_date in end_dates:
+            cf = [t for t in cashflows if t[0] >= start_date and t[0] <= end_date]
+            # add all balances on start date (but negated)
+            cf.extend([[b[0], -b[1]] for b in balances if b[0] == start_date])
+            # add all balances on end date
+            cf.extend([b for b in balances if b[0] == end_date])
+            results.append([start_date, end_date, fun(cf)])
+            start_date = end_date
     return results
 
 def main(argv):
@@ -33,7 +36,7 @@ def main(argv):
                 balances.append([txn_date, balance])
 
         
-    get_dates(cashflows, balances, financial.xirr)
+    print(get_dates(cashflows, balances, financial.xirr))
     cashflows.append(balances[-1])
     print(financial.xirr(cashflows))
 
